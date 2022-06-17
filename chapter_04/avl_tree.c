234ff4e65044363b6703e8d09e42f75650207954
index003 <index003@sina.cn>
2022-06-17 15:16:01 +0800

    fixed avl tree bug

--- chapter_04/avl_tree.c
+++ chapter_04/avl_tree.c
@@ -15,12 +15,12 @@ int main() {
     AvlTree node;
     AvlTree root;
     root = insert_tree(6, NULL);
-    insert_tree(4, root);
-    //insert_tree(3, root);
-    //insert_tree(2, root);
-    //insert_tree(1, root);
-    //insert_tree(8, root);
-
+    root = insert_tree(4, root);
+    root = insert_tree(3, root);
+    root = insert_tree(2, root);
+    root = insert_tree(1, root);
+    root = insert_tree(8, root);
+    printf("root node is : %d\n", root -> element);
     print_tree(root);
     printf("=============\n");
 
@@ -35,10 +35,16 @@ int main() {
     max = find_max(root);
     printf("max node element is : %d\n", max->element);
     printf("=============\n");
+
+    delete_tree(3, root);
+    printf("root node is : %d\n", root -> element);
+    print_tree(root);
+    printf("=============\n");
 }
 
 
 static int get_height(Position p) {
+
     if (p == NULL) {
         return -1;
     } else {
@@ -91,7 +97,7 @@ int max (int a, int b) {
 /* Perform a rotate between a node (k2) and its left child */
 /* Update heights, then return new root */
 
-static Position single_rotate_with_left(Position k2) {
+Position single_rotate_with_left(Position k2) {
     
     Position k1;
     k1 = k2->left;
@@ -100,7 +106,6 @@ static Position single_rotate_with_left(Position k2) {
 
     k2->height = max(get_height(k2->left), get_height(k2->right)) + 1;
     k1->height = max(get_height(k1->left), k2->height) + 1;
-
     return k1;  /* new root */
 }
 
@@ -175,13 +180,34 @@ AvlTree insert_tree(ElementType x, AvlTree t) {
     /* else x is in the tree already; we'll do nothing */
 
     t->height = max(get_height(t->left), get_height(t->right)) + 1;
-
     return t;
 }
-/*
+
 AvlTree delete_tree(ElementType x, AvlTree t) {
+    
+    Position tmp;
+    if (t == NULL) {
+        printf("empty tree!!\n");
+        return NULL;
+    } else if (x < t->element) {
+        t->left = delete_tree(x, t->left);
+    } else if (x > t->element) {
+        t->right = delete_tree(x, t->right);
+    } else if (t->left != NULL && t->right != NULL) {
+        tmp = find_min(t->right);
+        t->element = tmp->element;
+        t->right = delete_tree(t->element, t->right);
+    } else {
+        tmp = t;
+        if (t->left == NULL) {
+            t = t->right;
+        } else if (t->right == NULL) {
+            t = t->left;
+        }
+        free(tmp);
+    }
+    return t;
 }
-*/
 
 void print_tree(AvlTree t) {
 
